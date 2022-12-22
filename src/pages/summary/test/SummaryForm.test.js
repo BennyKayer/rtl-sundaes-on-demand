0af4f3917a354fcb1cc6b44a214194b788e1773d
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-12-22 20:41:11 +0100

    #04.44 Testing Element is not on Page: Popover Tests

--- src/pages/summary/test/SummaryForm.test.js
+++ src/pages/summary/test/SummaryForm.test.js
@@ -28,10 +28,21 @@ test("Test Summary form", async () => {
 
 test("Popover response to hover", async () => {
     const user = userEvent.setup();
+    render(<SummaryForm />);
 
     // Popover is hidden initially
+    const nullPop = screen.queryByText(
+        /no ice cream will actually be delivered/i
+    );
+    expect(nullPop).not.toBeInTheDocument();
 
     // Popover appears on mouseover of checkbox label
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+    await userEvent.hover(termsAndConditions);
+    const pop = screen.getByText(/no ice cream will actually be delivered/i);
+    expect(pop).toBeInTheDocument();
 
     // Popover disappears on mouseout
+    await userEvent.unhover(termsAndConditions);
+    expect(pop).not.toBeInTheDocument();
 });
