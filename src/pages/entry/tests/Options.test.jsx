8c0e7b453ee64d1cac3e9e70ef630adb9b1def21
Paweł Benkowski <pawlo97.pb@gmail.com>
2023-01-09 17:32:11 +0100

    #05.56 Code Quiz, toppings options from server

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -12,3 +12,19 @@ test("displays image for each scoop from the server", async () => {
     const altTexts = scoopImages.map((el) => el.getAttribute("alt"));
     expect(altTexts).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+
+test("displays image of each topping from the server", async () => {
+    render(<Options optionType="toppings" />);
+
+    const toppingsImages = await screen.findAllByRole("img", {
+        name: /topping$/i,
+    });
+    expect(toppingsImages).toHaveLength(3);
+
+    const altTexts = toppingsImages.map((el) => el.getAttribute("alt"));
+    expect(altTexts).toEqual([
+        "Cherries topping",
+        "M&Ms topping",
+        "Hot fudge topping",
+    ]);
+});
