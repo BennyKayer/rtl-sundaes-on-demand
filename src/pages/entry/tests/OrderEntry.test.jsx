8ec1708f7f02c7a7e9b5452ab9f341f6bbcb2177
Paweł Benkowski <pawlo97.pb@gmail.com>
2023-01-11 18:11:06 +0100

    #05.61 Running only selected tests and waitFor

--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -1,8 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import OrderEntry from "../OrderEntry";
 import { rest } from "msw";
 import { server } from "../../../mocks/server";
 
+// test.only will run only this single test ignoring the rest
+// test.skip will do the opposite
 test("handles error for scoops and toppings routes", async () => {
     server.resetHandlers(
         rest.get("http://localhost:3030/scoops", (req, res, ctx) =>
@@ -15,9 +17,12 @@ test("handles error for scoops and toppings routes", async () => {
 
     render(<OrderEntry />);
 
-    const alerts = await screen.findAllByRole("alert", {
-        name: "An unexpected error ocurred. Please try again later.",
-    });
+    // 2 alerts for both request, race condition may happen
+    // const alerts = await screen.findAllByRole("alert");
+    // https://testing-library.com/docs/dom-testing-library/api-async/#waitfor
 
-    expect(alerts).toHaveLength(2);
+    await waitFor(async () => {
+        const alerts = await screen.findAllByRole("alert");
+        expect(alerts).toHaveLength(2);
+    });
 });
