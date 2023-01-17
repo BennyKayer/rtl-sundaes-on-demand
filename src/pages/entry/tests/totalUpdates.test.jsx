52045cf8575e7fbfecd4917b5c3df67b61fb4c51
Paweł Benkowski <pawlo97.pb@gmail.com>
2023-01-17 20:48:32 +0100

    #06.70 Creating Custome Render to Wrap in Provider by default

--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,16 +1,15 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../../../test-utils/testing-library-utils";
 import userEvent from "@testing-library/user-event";
-import { OrderDetailsProvider } from "../../../contexts/OrderDetails";
 import Options from "../Options";
 
 test("update scoop subtotal when scoops changes", async () => {
     const user = userEvent.setup();
-    render(<Options optionType={"scoops"} />, {
-        wrapper: OrderDetailsProvider,
-    });
+    render(<Options optionType={"scoops"} />);
 
     // subtotal start at $0.00
-    const scoopSubtotal = screen.getByText("Scoops total: $", { exact: false });
+    const scoopSubtotal = screen.getByText("Scoops total: $", {
+        exact: false,
+    });
     expect(scoopSubtotal).toHaveTextContent("$0.00");
 
     // update vanilla scoops to 1, and check subtotal
@@ -22,10 +21,10 @@ test("update scoop subtotal when scoops changes", async () => {
     expect(scoopSubtotal).toHaveTextContent("$2.00");
 
     // update chocolate scoops to 2 and check subtotal
-    const chocolateScoops = await screen.getByRole("spinbutton", {
+    const chocolateInput = await screen.findByRole("spinbutton", {
         name: "Chocolate",
     });
-    await user.clear(chocolateScoops);
-    await user.type(chocolateScoops, "2");
-    expect(chocolateScoops).toHaveTextContent("$6.00");
+    await userEvent.clear(chocolateInput);
+    await userEvent.type(chocolateInput, "2");
+    expect(scoopSubtotal).toHaveTextContent("$24.00");
 });
