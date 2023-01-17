ba3b56cfb5e780c279468d6118a6e9628310be0c
Paweł Benkowski <pawlo97.pb@gmail.com>
2023-01-17 21:11:39 +0100

    #06.72 Code Quiz: Toppings Subtotal

--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -28,3 +28,39 @@ test("update scoop subtotal when scoops changes", async () => {
     await userEvent.type(chocolateInput, "2");
     expect(scoopSubtotal).toHaveTextContent("$24.00");
 });
+
+test("update topping subtotal based on checked or unchecked toppings options", async () => {
+    const user = userEvent.setup();
+    render(<Options optionType="toppings" />);
+    // Toppings are $1.50 each
+    // initially no toppings are checked
+    const toppingSubtotal = screen.getByText("Toppings total: $", {
+        exact: false,
+    });
+    expect(toppingSubtotal).toHaveTextContent("$0.00");
+
+    // Check cherries check if subtotal properly updated
+    const cherriesCheckbox = await screen.findByRole("checkbox", {
+        name: "Cherries",
+    });
+    await user.click(cherriesCheckbox);
+    expect(toppingSubtotal).toHaveTextContent("$1.50");
+
+    // Check mms check if subtotal properly updated
+    const mmsCheckbox = await screen.findByRole("checkbox", {
+        name: "M&Ms",
+    });
+    await user.click(mmsCheckbox);
+    expect(toppingSubtotal).toHaveTextContent("$3.00");
+
+    // Finally check the last one then check subtotal
+    const hotFudgeCheckbox = await screen.findByRole("checkbox", {
+        name: "Hot fudge",
+    });
+    await user.click(hotFudgeCheckbox);
+    expect(toppingSubtotal).toHaveTextContent("$4.50");
+
+    // Uncheck mms and check whether subtotal adjusted
+    await user.click(mmsCheckbox);
+    expect(toppingSubtotal).toHaveTextContent("$3.00");
+});
